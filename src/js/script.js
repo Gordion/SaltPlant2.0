11526d2fc7be6d409a87d155e05b5a452a8da10e
Gordion <darthgordion@gmail.com>
2020-05-05 21:03:45 +0300

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -53,6 +53,62 @@ $(document).ready(function(){
     console.log(window.location.href);
   }
 
+  // function hoverLike(element) {
+  //   element.setAttribute('src', 'images/liked.svg');
+  // }
+  //
+  // function unhoverLike(element) {
+  //   element.setAttribute('src', 'images/like.svg');
+  // }
+  //
+  // function hoverShare(element) {
+  //   element.setAttribute('src', 'images/shared.svg');
+  // }
+  //
+  // function unhoverShare(element) {
+  //   element.setAttribute('src', 'images/shareIndex.svg');
+  // }
+
+  $(".like-button").on("mouseover", function (element) {
+    $(".like-button").attr("src","images/liked.svg");
+});
+
+$(".like-button").on("mouseout", function (element) {
+  $(".like-button").attr("src","images/like.svg");
+});
+
+$(".share-button").on("mouseover", function (element) {
+  $(".share-button").attr("src","images/shared.svg");
+});
+
+$(".share-button").on("mouseout", function (element) {
+  $(".share-button").attr("src","images/shareIndex.svg");
+});
+
+$(".like-button-mp").on("mouseover", function (element) {
+  $(".like-button-mp").attr("src","images/liked.svg");
+});
+
+$(".like-button-mp").on("mouseout", function (element) {
+$(".like-button-mp").attr("src","images/like-mp.svg");
+});
+
+$(".share-button-mp").on("mouseover", function (element) {
+$(".share-button-mp").attr("src","images/shared.svg");
+});
+
+$(".share-button-mp").on("mouseout", function (element) {
+$(".share-button-mp").attr("src","images/share-mp.svg");
+});
+
+$(".like-min-slider").on("mouseover", function (element) {
+$(event.target).attr("src","images/liked-white.svg");
+});
+
+$(".like-min-slider").on("mouseout", function (event) {
+  $(event.target).attr("src","images/like.svg");
+});
+
   // function cost() {
   //  var price = 200;
   //  var total = price *
@@ -87,6 +143,9 @@ $(document).ready(function(){
 
   $('.buy-excursion').stickySidebar({
     topSpacing: 60,
-    bottomSpacing: 60
+    bottomSpacing: 60,
+    innerWrapperSelector: '.basic-placeholder-spc',
+    minwidth: 470
+
   });
 });
